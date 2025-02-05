67dcd3e17aff8c3d5a8bc92f747c3622ed78cfc8
mxber2022 <mxber2022@gmail.com>
2025-02-05 18:08:51 +0100

    duplicate index removal

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useAppKitAccount } from '@reown/appkit-ethers5-react-native'
 
 export default function ProfileScreen() {
+  const { address, chainId, isConnected } = useAppKitAccount()
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Profile Screen</Text>
+      <Text>{address}</Text>
     </View>
   );
 }
